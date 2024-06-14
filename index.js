89bbe258c2540880a0156cc2c57121fc7d4bf3e8
Yehor Tkachuk <tkachuk.egor2010@gmail.com>
2024-06-14 22:28:24 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+
+
+// ПЕРШЕ ЗАВДАННЯ 
+
+// function logItems(array) {
+//     for (let i = 0; i < array.length; i++) {
+//       console.log(`${i + 1} - ${array[i]}`);
+//     }
+//   }
+
+//   let itemsList = ['Mango', 'Poly', 'Ajax'];
+//   logItems(itemsList);
+  
+// ДРУГЕ ЗАВДАННЯ
+
+// function calculateEngravingPrice(message, pricePerWord){
+
+//    let words = message.split(' ') 
+
+
+//    let numberOfWords = words.length 
+//    let totalPrice = numberOfWords * pricePerWord
+//    return totalPrice
+// }
+
+// let writeMessage =prompt("введіть слова що ви хочете вигравіювати")
+// let wordPrice = 1918 
+
+// console.log(calculateEngravingPrice(writeMessage , wordPrice))
+
+// ЗАВДАННЯ 3
+
+// function findLongestWord(string){
+
+//     let words = string.split (' ')
+
+//  let longestWord = (words[0])
+
+//  for (word of words){
+
+//  if (word.length > longestWord.length){
+//     longestWord = word 
+//  }
+
+//     }
+
+// return longestWord
+
+
+// }
+
+// let something = prompt("Введіть слова")
+// console.log(findLongestWord(something))
+
+// ЗАВДАННЯ 4 
+
+// function formatString(string){
+
+
+// let stringLength = string.length
+
+// let maxLength = 40
+
+// if ( stringLength > maxLength ){
+//    return  string.slice( 0 , maxLength  ) + "..."
+// } else {return string}
+
+
+
+// }
+
+// let something = prompt("Введіть щось")
+// console.log( formatString(something))
+
+// ЗАВДАННЯ 5 
+
+// function checkForSpam(message){
+
+// if (message.includes("spam")|| message.includes("sale")){
+//     return true 
+// }
+// else {return false}
+
+// }
+
+// let something = prompt("Введіть щось")
+// console.log(checkForSpam(something))
+
+// ЗАВДАННЯ 6 
